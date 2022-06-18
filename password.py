80aa2c7ac451c2dbee6daa94db353c7b661935ad
HPScoding <96297528+HPScoding@users.noreply.github.com>
2022-06-18 15:24:59 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,20 @@
+import numbers
+import random
+
+print('Welcome to your Password Generator')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+?/\|[],.<>;:`~1234567890'
+
+number = input('Amount of passwords to generate: ')
+number = int(number)
+
+length = input('Input your password length: ')
+length = int(length)
+
+print('\nhere are your passwords: ')
+
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)    
